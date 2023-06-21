ed669b5828ea8cb40d0347c771c38c64fdc09c5a
Kaushal Bhardwaj <74184474+RockeTCodes@users.noreply.github.com>
2023-06-21 12:31:51 +0530

    added snake game(oops) , listeners etc.

---
+++ intermediate/Snake Game/scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 260)
+        self.write(f"Score: {self.score}",
+                   align="center", font=("Ariel", 24, "normal"))
+
+    def inc_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}",
+                   align="center", font=("Ariel", 24, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over",
+                   align="center", font=("Ariel", 24, "normal"))
---
+++ intermediate/Snake Game/snake_game_oops.py
@@ -0,0 +1,52 @@
+from turtle import Turtle, Screen
+from time import sleep
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+STEP = 20
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.new_snake()
+
+    def new_snake(self):
+        for positions in STARTING_POSITIONS:
+            segment = Turtle("square")
+            segment.color("white")
+            segment.penup()
+            segment.goto(positions)
+            self.segments.append(segment)
+
+    def move_snake(self):
+        for segment in range(len(self.segments)-1, 0, -1):
+            new_x = self.segments[segment-1].xcor()
+            new_y = self.segments[segment-1].ycor()
+            self.segments[segment].goto(new_x, new_y)
+        self.segments[0].forward(STEP)
+
+    def add_segment(self, position):
+        segment = Turtle("square")
+        segment.color("white")
+        segment.penup()
+        segment.goto(position)
+        self.segments.append(segment)
+
+    def increase_snake(self):
+        self.add_segment(self.segments[-1].position())
+
+    def up(self):
+        if self.segments[0].heading() != 270:
+            self.segments[0].setheading(90)
+
+    def down(self):
+        if self.segments[0].heading() != 90:
+            self.segments[0].setheading(270)
+
+    def left(self):
+        if self.segments[0].heading() != 0:
+            self.segments[0].setheading(180)
+
+    def right(self):
+        if self.segments[0].heading() != 180:
+            self.segments[0].setheading(0)
